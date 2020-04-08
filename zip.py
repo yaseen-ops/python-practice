f27f192850da49f0a5d4e013b4046d400cf4b178
yaseen-ops <mohammedbyasin@gmail.com>
2020-04-08 10:42:13 -0700

    zip code file added

---
+++ zip.py
@@ -0,0 +1,9 @@
+Names = ['Dhoni', 'Federrer', 'Bolt']
+Prof = ['Cricket', 'Tennis', 'Running']
+
+zipped = list(zip(Names, Prof))
+print(zipped)
+
+#zipped = zip(Names, Prof)
+#for (a,b) in zipped:
+#    print(a,b)
